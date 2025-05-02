bb9eca626549e0d35786ecb1afc3332c052c91c2
apple <yzt238910@163.com>
2025-05-02 17:00:55 +0800

    end

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller"
 import { UserController } from "./user.controller"
+import {loggerService } from "./logger.service"
+
 
 @Module({
     controllers: [AppController, UserController]
